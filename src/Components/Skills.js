d45f5ea2c7ad984dbac032647a0e2ebd6a59d07c
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-07-22 17:01:03 +0530

    skill section images added

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import javascriptImg from '../img/javascript.png'
+import reactImg from '../img/react.png'
+import reactRouterImg from '../img/react-router.png'
+import reduxImg from '../img/redux.png'
 
 const Skills = () => {
     return (
-        <div>
-            Skills
-        </div>
+        <main className="slills-container">
+            <div className="skills-card">
+                <p>
+                    <span>Javascript</span>
+                </p>
+                <img src={javascriptImg} alt="" />
+            </div>
+            <div className="skills-card">
+                <p>
+                    <span>React Js</span>
+                </p>
+                <img src={reactImg} alt="" />
+            </div>
+            <div className="skills-card">
+                <p>
+                    <span>React Router</span>
+                </p>
+                <img src={reactRouterImg} alt="" />
+            </div>
+            <div className="skills-card">
+                <p>
+                    <span>Redux : State managment</span>
+                </p>
+                <img src={reduxImg} alt="" />
+            </div>
+
+        </main>
     )
 }
 
