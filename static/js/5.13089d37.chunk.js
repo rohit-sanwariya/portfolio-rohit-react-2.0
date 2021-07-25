d5943ad0feaa089a2a739f6824ac1e0e1fd13985
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-07-26 00:02:34 +0530

    Updates

---
+++ static/js/5.13089d37.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprohit-portfolio-react2.0"]=this["webpackJsonprohit-portfolio-react2.0"]||[]).push([[5],{38:function(t,i,r){"use strict";r.r(i);r(0);var s=r.p+"static/media/work-illustrations.254a0f50.png",a=r(1);i.default=function(){return Object(a.jsx)("div",{className:"card",children:Object(a.jsx)("img",{src:s,alt:"",className:"work-ilustration"})})}}}]);
+//# sourceMappingURL=5.13089d37.chunk.js.map
